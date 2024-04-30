97c9a9bc0a69195ae5615351c127a142e998d216
Marina Mel <melm2004@mail.ru>
2024-04-30 10:26:50 +0300

    update message of error

--- mel-vite/src/Pages/ErrorPage.tsx
+++ mel-vite/src/Pages/ErrorPage.tsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom"
 export function ErrorPage(){
     let {id} = useParams();
     return(
-        <h1>Ошибка {id}</h1>
+        <>
+            <h1>{id}</h1>
+            <h6>Сейчас что-то не так, но рано или поздно всё заработает.</h6>
+        </>
     )
 }
